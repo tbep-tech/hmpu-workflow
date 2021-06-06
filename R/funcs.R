46e37195eeee0cd9f3f80b69b3dbdd1f783a2e4a
fawda123 <mbafs2012@gmail.com>
2021-06-06 11:31:19 -0400

    g_legend function to funcs

--- R/funcs.R
+++ R/funcs.R
@@ -1,3 +1,11 @@
+# extract legend from existing ggplot object
+g_legend <- function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
 # estimate lulc area in acres for available HMPU targets 
 #
 # lulcin in lulc rdata file for a given year
